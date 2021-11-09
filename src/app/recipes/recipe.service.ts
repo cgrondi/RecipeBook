b5798a9f311625a656c3a402a87d142947ce622c
Cameron Grondines <camerongrondines@Camerons-MacBook-Pro.local>
2021-11-09 10:44:18 -0600

    change shopping-list.service eventEmitter to a Subject

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,4 @@
-import { EventEmitter, Injectable } from "@angular/core";
+import { Injectable } from "@angular/core";
 import { Ingredient } from "../shared/ingredient.model";
 import { ShoppingListService } from "../shopping-list/shopping-list.service";
 import { Recipe } from "./recipe.model";
@@ -32,7 +32,6 @@ export class RecipeService {
             ]
         )
     ];
-    recipeSelected = new EventEmitter<Recipe>();
 
     constructor(private shoppingListService: ShoppingListService) { }
 
